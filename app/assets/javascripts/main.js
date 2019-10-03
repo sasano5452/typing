06df1a73e94ec5e2d3b5d7b8b90552a42578f759
sasano5452 <arukara5452@gmail.com>
2019-10-03 17:17:16 +0900

    位置調整

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,17 +2,18 @@
 'use strict';
 
 window.onload = function(){
-  // 変数宣言
+
+// 変数宣言
   let words = [];
-  let word;
-  let loc;
+  let word;          
+  let loc;     // 文字が何番目かを管理したい 重要
   let score;
   let miss;
   const timeLimit = 15 * 1000;  //タイムリミット
   let startTime;
   let isPlaying = false;
   
-  //id要素の取得
+//id要素の取得
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
@@ -22,53 +23,12 @@ window.onload = function(){
   const umi = document.getElementById('umi');
   const result = document.getElementById('result');
 
-  // モーダルを消す処理
+// モーダルを消す処理
   mask.addEventListener('click', function () {
     modal.classList.add('hidden');
     mask.classList.add('hidden');
-    umi.classList.add('hidden');
   });
   
-
-  //入力した単語を_に変える
-  function updataTarget() {
-    let placeholder ='';
-    for (let i = 0; i < loc; i++){
-      placeholder += '_';
-    }
-    target.textContent = placeholder + word.substring(loc);
-  }
-
-  function updateTimer() {
-    const timeLeft = startTime + timeLimit - Date.now();
-    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
-    const timeoutId = setTimeout(() => {
-      updateTimer();
-        }, 10);
-    if (timeLeft < 0) {
-      isPlaying = false;
-      clearTimeout(timeoutId);
-      //モードルを出す処理
-      modal.classList.remove('hidden');
-      mask.classList.remove('hidden');
-      target.textContent = ''
-      document.getElementById("target").style.color = "";
-      timerLabel.textContent = '0.00'
-      setTimeout(() => {
-        showResult();
-      }, 100);
-      ruby.textContent = "ruby Replay"
-      start.textContent = "javascript play"
-    };
-  };
-
-  //正答率の計算    
-  function showResult() {
-    const accuracy = score + miss === 0 ? 0 :score / (score + miss) * 100;
-    console.log(score);   //クッキーをうちこめ
-    result.textContent = accuracy.toFixed(2)
-  };
-
 //クリックでrubyゲームスタート
   ruby.addEventListener('click', () =>{
     umi.classList.remove('hidden');
@@ -84,7 +44,6 @@ window.onload = function(){
     if (isPlaying === true) {
       return};
     isPlaying = true;  
-
     loc = 0;
     score = 0;
     miss = 0;
@@ -93,7 +52,7 @@ window.onload = function(){
     word = words[Math.floor(Math.random() * words.length)];
 
     target.textContent = word;
-    startTime = Date.now();
+    startTime = Date.now();     //現在時刻を取得
     updateTimer();
   })
 
@@ -129,9 +88,7 @@ window.onload = function(){
     if (isPlaying !== true) {
       return;
     }
-
-
-    if (e.key === word[loc]) {
+    if (e.key === word[loc]) {     //打ったキーとwordのloc番目の文字が同じなら
       loc++;
       document.getElementById("target").style.color = "";
       // 単語が正解なら次の単語に行く処理
@@ -139,7 +96,6 @@ window.onload = function(){
         word = words[Math.floor(Math.random() * words.length)];  // ランダムに単語をだす
         loc = 0;
       }
-
       updataTarget();
       score++;
       scoreLabel.textContent = score;
@@ -150,6 +106,46 @@ window.onload = function(){
       missLabel.textContent = miss;
     }
   });
-  
+
+  //入力した単語を_に変える (_と残りの文字を結合する)
+  function updataTarget() {
+    let placeholder ='';
+    for (let i = 0; i < loc; i++){
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+ //時間制限
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+   //時間を動かす 
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+        }, 10);
+    if (timeLeft < 0) {
+      isPlaying = false;
+      clearTimeout(timeoutId);
+    //モードルを出す処理
+      modal.classList.remove('hidden');
+      mask.classList.remove('hidden');
+      umi.classList.add('hidden');
+      target.textContent = ''
+      document.getElementById("target").style.color = "";
+      timerLabel.textContent = '0.00'
+      setTimeout(() => {
+        showResult();
+      }, 100);
+      ruby.textContent = "ruby Replay"
+      start.textContent = "javascript play"
+    };
+  };
+
+  //正答率の計算    
+  function showResult() {
+    const accuracy = score + miss === 0 ? 0 :score / (score + miss) * 100;
+    console.log(score);   //クッキーをうちこめ
+    result.textContent = accuracy.toFixed(2)
+  };
 };
 
